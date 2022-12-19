5f9d4378fb6d0418a7a6e1e15b9cd9af1fa09fe1
Indominable <54984502+EbioJosh@users.noreply.github.com>
2022-12-19 19:32:04 +0800

    Update CPE112A_EbioJosh_2.c

--- CPE112A_EbioJosh_2.c
+++ CPE112A_EbioJosh_2.c
@@ -2,32 +2,32 @@
 
 void power(int x, int y, double *p2)
 {
-	int i;	
-	double p=1;
-	for ( i=1; i<=y;  i++ )
-	{
-		p = p * x;
-	}
-	*p2=p;
-}	
+    double result = 1.0;
+    int i;
 
-int main(){	
-
-	int x, y;
-	double p1;
-	
-	printf("Please input the first integer (base): ");
-	scanf("%d",&x);
-	if (x<0)
-    {
-        printf("\n Please input a positive integer.");
+    if (y >= 0) {
+        for (i = 0; i < y; i++)
+            result *= x;
+    } else {
+        for (i = 0; i < -y; i++)
+            result /= x;
     }
-    else{
-    printf("\nPlease input the second integer (exponent): ");
-	scanf("%d",&y);
-	
-	power(x,y,&p1);
-	printf("\n%d^%d = %.2lf\n", x, y, p1);
-	return 0;
-	}
+
+    *p2 = result;
+}
+
+int main()
+{
+    int x, y;
+    double p1;
+
+    printf("Enter the base: ");
+    scanf("%d", &x);
+    printf("Enter the exponent: ");
+    scanf("%d", &y);
+
+    power(x, y, &p1);
+    printf("%d^%d = %.2lf\n", x, y, p1);
+
+    return 0;
 }
